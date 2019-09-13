cd6fada0df10210c5b3a52aeb3eb6f24d072fed4
Administrator <administrator@CN-6.local>
2019-09-13 15:42:33 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,43 +1,96 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 
 
-class App extends React.Component { 
-  render() 
-  { 
-  return ( 
-  <div className="frame"> 
-  <div class="mainCalc"> 
-  <div className="button-row"> 
-    <Button label={'Clear'}/> 
-    <Button label={'Delete'}/> 
-    <Button label={'.'}/> 
-    <Button label={'/'}/> 
-  </div> 
-  <div className="button-row"> 
-    <Button label={'7'}/> 
-    <Button label={'8'}/> 
-    <Button label={'9'}/> 
-    <Button label={'*'}/> 
-  </div> 
-  <div className="button-row"> 
-    <Button label={'4'}/> 
-    <Button label={'5'}/> 
-    <Button label={'6'}/> 
-    <Button label={'-'}/> 
-  </div> 
-  <div className="button-row"> 
-    <Button label={'1'}/> 
-    <Button label={'2'}/> 
-    <Button label={'3'}/> 
-    <Button label={'+'}/> 
-  </div> 
-  <div className="button-row"> 
-    <Button label={'0'}/> 
-    <Button label={'='}/> 
-  </div> 
-  </div> 
-  </div> 
-  ); 
-} 
-} 
+
+
+
+class App extends React.Component {
+    
+  constructor(props) {
+    super(props);
+    this.state={phone:[]};
+  }
+  
+  save() {
+    var phone = [...this.state.phone];
+    phone.push(this.newText.value);
+    this.setState({phone});
+  }
+
+  render(){
+      return(
+          <div className="list">
+            <h1> Phonebook</h1>
+            <input type="text" ref={(ip) => {this.newText = ip}}/>
+            <button onClick={this.save.bind(this)} className="button1">Save
+            </button>
+            <hr></hr>
+            <ul className= 'lists'>
+              {this.state.phone.map(function(todo) {
+                    return <li>+44 {todo}</li>
+               })}
+              
+            </ul>
+          </div>
+      )
+  }
+};
+export default App
+// ReactDOM.render(<Todo/>, document.getElementById('app'));
+
+
+
+
+
+
+
+
+
+// import React, {Component} from 'react';
+// import './App.css';
+
+
+
+
+
+
+
+
+
+// class App extends Component  {
+//   constructor(){
+//     super()
+//     this.state ={
+//     phoneNumber: null
+//     }
+//     }
+// handleSubmit= (event)=>{
+//   event.preventDefault();
+// }
+// handleInputChange = (event) =>{
+//   event.preventDefault();
+//  this.setState ({
+//    [event.target.name]:event.target.value
+//  })
+//   }
+
+// render() {
+//   const {phoneNumber} = this.state
+//   return(
+//  <div>  
+//  <form onSubmit= {this.handleSubmit}>
+//  <input type = "text" 
+//  name= 'phoneNumber'
+//  placeholder="Phone Number"  onChange= {this.handleInputChange} /> 
+//   <button>Sumbit</button>
+//    <hr/>
+//    <h2>Phone Number List</h2></form>
+//    <p></p>
+// </div>
+//   )}
+// }
+
+
+
+// export default App
