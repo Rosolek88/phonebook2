ced26da71f0e2df149ca52ec335f80643c824985
System Administrator <root@CN-6.local>
2019-09-13 16:17:06 +0100

    Updates

---
+++ precache-manifest.3dcee1dd73dd51d714563a4b81ac2fc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd126ce915591246244b2a49a482691c",
+    "url": "/phonebook2/index.html"
+  },
+  {
+    "revision": "08ad545e49842db4f4e7",
+    "url": "/phonebook2/static/css/main.403783f8.chunk.css"
+  },
+  {
+    "revision": "8cd3b827dd65557d8a7c",
+    "url": "/phonebook2/static/js/2.189a2b85.chunk.js"
+  },
+  {
+    "revision": "08ad545e49842db4f4e7",
+    "url": "/phonebook2/static/js/main.f3348b67.chunk.js"
+  },
+  {
+    "revision": "8de085ebef07d1bf6996",
+    "url": "/phonebook2/static/js/runtime~main.f7728715.js"
+  }
+]);
